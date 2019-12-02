3cc34ffd0df6d11f325d7d71a362ca759c411a41
zurisR <zharofmaxim@mail.ru>
2019-12-02 14:40:32 +0300

    Updates

---
+++ precache-manifest.454c952a509954f3ef460056fb3898ca.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88d160ed268a57f88dbb0c3e232bed79",
+    "url": "/SimpleSnake/index.html"
+  },
+  {
+    "revision": "ddd8a893238b856652e1",
+    "url": "/SimpleSnake/static/css/main.02e46b27.chunk.css"
+  },
+  {
+    "revision": "931859bf2f249f353e97",
+    "url": "/SimpleSnake/static/js/2.f15449cd.chunk.js"
+  },
+  {
+    "revision": "ddd8a893238b856652e1",
+    "url": "/SimpleSnake/static/js/main.9cfa59ae.chunk.js"
+  },
+  {
+    "revision": "4dba23292bd5bc571d44",
+    "url": "/SimpleSnake/static/js/runtime-main.1a51c530.js"
+  }
+]);
